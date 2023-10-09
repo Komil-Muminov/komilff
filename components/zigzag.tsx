5dbbfe3d3a8f5b96afc3f8287f314f50a582e765
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-10-09 16:51:05 +0500

    changed text in hobbie

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -4,7 +4,6 @@ import FeatImage01 from '@/public/images/features-03-image-01.jpg';
 import FeatImage02 from '@/public/images/features-03-image-02.jpeg';
 import FeatImage04 from '@/public/images/features-03-image-03.jpeg.jpg';
 
-
 export default function Zigzag() {
 	return (
 		<section>
@@ -172,9 +171,7 @@ export default function Zigzag() {
 												xmlns='http://www.w3.org/2000/svg'>
 												<path d='M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z' />
 											</svg>
-											<span>
-												Смешанные боевые искусства{' '}
-											</span>
+											<span>Саморазвитие </span>
 										</li>
 										<li className='flex items-center'>
 											<svg
