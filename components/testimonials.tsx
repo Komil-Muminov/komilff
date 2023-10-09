6383e59d745a07f3a8ccdd8c1d9bb87bc34cd16d
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-10-09 15:20:45 +0500

    corrected data-aos

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -24,7 +24,7 @@ export default function Testimonials() {
 					</div> */}
 					<div
 						className='flex items-center justify-center '
-						data-aos='fade-rigth'>
+						data-aos='fade-right'>
 						<div className='w-1/2'>
 							{
 								<Image
