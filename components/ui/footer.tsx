375f2fa47e92cab99a481b4a5772839b9ec7a4c1
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-10-13 10:12:26 +0500

    installed material ui & material icons

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,14 +1,14 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
-
-
+import { Mail, Instagram, Facebook } from '@mui/icons-material';
 export default function Footer() {
 	return (
 		<footer>
 			<div className='py-12 md:py-16'>
 				<div className='max-w-6xl mx-auto px-4 sm:px-6'>
 					<div className='md:flex md:items-center md:justify-center'>
-						
 						{/* <div className='text-gray-400 text-sm mr-4 text-center'>
 							<small className='text-red-300 duration-300 ease-in-out hover:text-red-400 '>
 								{' '}
@@ -20,46 +20,43 @@ export default function Footer() {
 								<p>Мой инстаграм:</p>
 
 								<Link
+									target='_blank'
 									href='https://www.instagram.com/komilff/'
 									className='flex justify-center items-center text-purple-600 bg-gray-800 duration-300 ease-in-out hover:text-gray-100 hover:bg-purple-600 rounded-full '
 									aria-label='Instagram'>
-									<svg
-										className='w-8 h-8 fill-current'
-										viewBox='0 0 32 32'
-										xmlns='http://www.w3.org/2000/svg'>
-										<circle
-											cx='20.145'
-											cy='11.892'
-											r='1'
-										/>
-										<path d='M16 20c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2z' />
-										<path d='M20 24h-8c-2.056 0-4-1.944-4-4v-8c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zm-8-14c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2v-8c0-.935-1.065-2-2-2h-8z' />
-									</svg>
+									<Instagram />
 								</Link>
 							</li>
 							<li className='ml-4'>
 								<p>Мой фейсбук:</p>
 
 								<Link
+									target='_blank'
 									href='https://facebook.com/profile.php?id=100011337580298&_se_imp=1YoEl3FbgNNKkDUwT'
 									className='flex justify-center items-center text-purple-600 bg-gray-800   duration-300 ease-in-out hover:text-gray-100 hover:bg-purple-600 rounded-full '
 									aria-label='Facebook'>
-									<svg
-										className='w-8 h-8 fill-current'
-										viewBox='0 0 32 32'
-										xmlns='http://www.w3.org/2000/svg'>
-										<path d='M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z' />
-									</svg>
+									<Facebook />
+								</Link>
+							</li>
+
+							<li className='ml-4'>
+								<p>Написать мне:</p>
+
+								<Link
+									target='_blank'
+									href='https://docs.google.com/forms/d/e/1FAIpQLSfZMHCZ6q3kblUIamI9QMwqrL-JIwKolEC9Ax-23kDL49HBpQ/viewform?usp=sf_link'
+									className='flex justify-center items-center text-purple-600 bg-gray-800   duration-300 ease-in-out hover:text-gray-100 hover:bg-purple-600 rounded-full '
+									aria-label='Facebook'>
+									<Mail />
 								</Link>
 							</li>
 						</ul>
 					</div>
-					
 				</div>
 				<div className='text-gray-400 text-sm mr-4 text-center mt-2'>
 					<small className='text-red-300 duration-300 ease-in-out pl-12 hover:text-red-400 '>
 						{' '}
-						forever in my mind 01.04 Лалила.
+						01.04 Лалила.
 					</small>
 				</div>
 			</div>
