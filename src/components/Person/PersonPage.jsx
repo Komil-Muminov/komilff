acff56a81eb2a91ceea22305349311e06e01a7ab
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-12-01 09:22:21 +0500

    fx

--- src/components/Person/PersonPage.jsx
+++ src/components/Person/PersonPage.jsx
@@ -1,5 +1,6 @@
 import "./css/Person.css";
 import Person from "./img/person.png";
+import KM from "./img/KM.png";
 import HtmlIcon from "./img/html5.svg";
 import Css from "./img/css.svg";
 import JS from "./img/JS.svg";
@@ -11,10 +12,14 @@ const PersonPage = () => {
 				<div className="container">
 					{/* PERSON */}
 
-					<div className="img-wrapper flex justify-center items-center">
-						<img src={Person} alt="" className="avatar-wrapper" />
+					<div className="img-wrapper flex justify-center items-center ">
+						<img
+							src={KM}
+							alt="km"
+							className="object-cover h-[19em] w-[19em] avatar-wrapper"
+						/>
 					</div>
-					<div className="text-wrapper flex justify-center items-center flex-col flex-wrap">
+					<div className="text-wrapper flex justify-center items-center flex-col flex-wrap rounded-md mb-[1.3em]">
 						<h1 className="sections-title person-title text-center text-2xl first-letter: md:text-6xl">
 							I'M KOMIL MUMINOV.
 						</h1>
