0c19339744c2d0886694c8be9e12b87bcbe65167
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-12-04 09:17:39 +0500

    retexed pastLive

--- src/components/PastLive/Pastlives.jsx
+++ src/components/PastLive/Pastlives.jsx
@@ -3,19 +3,18 @@ import React from "react";
 const Pastlives = () => {
 	return (
 		<>
-			<section className="sections experience-section ">
+			<section className="sections experience-section">
 				<div className="container">
 					<h2 className="sections-title text-center mb-[1em]">Past Live</h2>
 					<div className="text-wrapper flex flex-col justify-center items-center px-[1.1em]">
 						<p className="sections-desc mb-[1em]">
-							In the past, there was an incredible story in which a special
-							bond, standing on wet ground despite the absence of rain, came to
-							an end, opening a new beginning. This moment proved to be a
-							significant step for this story, and it moved on to the next
-							chapter of its life, leaving the past behind. And while the new
-							chapter in its life provided new opportunities, it will always
-							hold memories of the past in its heart. The name of this story is
-							Lalila.
+							Once there was a remarkable story about an unbreakable bond.
+							However, at some point, this bond, which seemed eternal, reached
+							its end, ushering in a new chapter. This transition became a
+							critical juncture for our tale, and it moved on to the next phase
+							of its existence, leaving the past behind. The new chapter brings
+							fresh opportunities, but memories of the past will always remain
+							in our hearts. We refer to this story as “Lalila.”
 						</p>
 					</div>
 				</div>
