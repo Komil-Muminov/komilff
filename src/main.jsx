4b70b5b9f217ce9c78a8d8be032dd22912110e4d
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-02-15 08:36:04 +0500

    fx responsive mobile

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import "./Media.css";
+import "./Media.css";
 
 import { BrowserRouter } from "react-router-dom";
 
